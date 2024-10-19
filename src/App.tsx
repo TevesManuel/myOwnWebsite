120813a4b7a65a2df8890d8fc3ce63c3eaf83fb8
Manuel Teves <manuel.tomas.teves@gmail.com>
2024-10-19 20:47:20 -0300

    Really setup for init the project

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,6 @@ function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <p>
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
             </header>
         </div>
     );
