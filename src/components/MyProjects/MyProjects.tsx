794ad4e1569640eb3680e3ddc689b97ad438e42d
Manuel Teves <manuel.tomas.teves@gmail.com>
2024-10-21 19:26:53 -0300

    Implementation of the list

--- src/components/MyProjects/MyProjects.tsx
+++ src/components/MyProjects/MyProjects.tsx
@@ -1,39 +1,37 @@
-import React, { useState } from "react";
+import React from "react";
 import "./MyProjects.css";
+import TSlider from "../TSlider/TSlider";
+import TSliderElement from "../TSlider/TSliderElement/TSliderElement";
 
 const MyProjects: React.FC = () => {
-  const [currentIndex, setCurrentIndex] = useState<number>(0);
-  const slides: string[] = ['Elemento 1', 'Elemento 2', 'Elemento 3', 'Elemento 4', 'Elemento 5', 'Elemento 6', 'Elemento 7'];
 
-  const handlePrev = () => {
-    setCurrentIndex((prevIndex) => (prevIndex > 0 ? prevIndex - 1 : slides.length - 1));
-  };
-
-  const handleNext = () => {
-    setCurrentIndex((prevIndex) => (prevIndex < slides.length - 1 ? prevIndex + 1 : 0));
-  };
 
   return (
     <div className="projectsMainContainer">
       <div className="projectsContainer">
-        <div className="slider">
-          <div
-            className="slides-container"
-            style={{ transform: `translateX(-${currentIndex === (slides.length-1) ? currentIndex*75-25 : currentIndex > 0 ? currentIndex*75-12.5 : 0}%)` }}
-            // style={{ transform: `translateX(-${currentIndex * (75) - 25}%)` }}
-          >
-            {slides.map((slide, index) => (
-              <div
-                key={index}
-                className={`slide ${index === currentIndex ? 'focus' : ''}`}
-              >
-                {slide}
-              </div>
-            ))}
-          </div>
-        </div>
-        <button onClick={handlePrev}>Anterior</button>
-        <button onClick={handleNext}>Siguiente</button>
+        <TSlider>
+            <TSliderElement>
+                <div className="fwh" style={{'backgroundColor': 'red'}}>
+                    <h1>
+                        Message 1
+                    </h1>
+                </div>
+            </TSliderElement>
+            <TSliderElement>
+                <div className="fwh" style={{'backgroundColor': 'green'}}>
+                    <h1>
+                        Message 2
+                    </h1>
+                </div>
+            </TSliderElement>
+            <TSliderElement>
+                <div className="fwh" style={{'backgroundColor': 'blue'}}>
+                    <h1>
+                        Message 3
+                    </h1>
+                </div>
+            </TSliderElement>
+        </TSlider>
       </div>
     </div>
   );
---
+++ src/components/TSlider/TSlider.tsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import "./TSlider.css";
+import TSliderElement from "./TSliderElement/TSliderElement";
+
+interface TSliderProps {
+    children: React.ReactNode;
+}
+  
+const TSlider: React.FC<TSliderProps> = ({ children }) => {
+    const [currentIndex, setCurrentIndex] = useState<number>(0);
+
+    const elementsCount = React.Children.count(children);
+
+    const slides = React.Children.map(children, (child) => {
+        if (React.isValidElement(child) && child.type === TSliderElement) {
+            return <div className="TSliderItem">{child}</div>;
+        }
+        return null;
+    }) || [];
+
+    const handlePrev = () => {
+        setCurrentIndex((prevIndex) => (prevIndex > 0 ? prevIndex - 1 : elementsCount - 1));
+    };
+
+    const handleNext = () => {
+        setCurrentIndex((prevIndex) => (prevIndex < elementsCount - 1 ? prevIndex + 1 : 0));
+    };
+
+    return (
+        <div style={{'width': '100%', 'height': '100%'}}>
+            <div className="slider">
+                <div
+                    className="slides-container"
+                    style={{ transform: `translateX(${currentIndex === (elementsCount-1) ? -(currentIndex*75-25+8) : currentIndex > 0 ? -(currentIndex*75-12.5) : 8}%)` }}
+                >
+                    {slides.map((slide, index) => (
+                    <div
+                        key={index}
+                        className={`slide ${index === currentIndex ? 'focus' : ''}`}
+                    >
+                        {slide}
+                    </div>
+                ))}
+            </div>
+            </div>
+                    
+            <button onClick={handlePrev}>Anterior</button>
+            <button onClick={handleNext}>Siguiente</button>
+        </div>
+    )
+}
+
+export default TSlider;
