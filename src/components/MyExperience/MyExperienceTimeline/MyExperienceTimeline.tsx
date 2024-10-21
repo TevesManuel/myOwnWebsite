d09a054627e2da54b7f7fb47ab48fbc5d4274b76
Manuel Teves <manuel.tomas.teves@gmail.com>
2024-10-21 11:24:39 -0300

    UNLP expercience added

--- src/components/MyExperience/MyExperienceTimeline/MyExperienceTimeline.tsx
+++ src/components/MyExperience/MyExperienceTimeline/MyExperienceTimeline.tsx
@@ -5,6 +5,8 @@ import TimelineSeparator from '@mui/lab/TimelineSeparator';
 import TimelineConnector from '@mui/lab/TimelineConnector';
 import TimelineContent from '@mui/lab/TimelineContent';
 import TimelineDot from '@mui/lab/TimelineDot';
+import Link from '@mui/material/Link';
+import "./MyExperienceTimeline.css";
 
 const MyExperienceTimeline : React.FC = () => {
   return (
@@ -19,7 +21,36 @@ const MyExperienceTimeline : React.FC = () => {
           <TimelineDot />
           <TimelineConnector />
         </TimelineSeparator>
-        <TimelineContent style={{'color': 'white'}}>Eat</TimelineContent>
+        <TimelineContent style={{'color': 'white'}}>
+          <div style={{'display': 'flex'}}>
+            <div style={{'flex': 5}}>
+              <h2 className='inter'>
+                UNITEC (UNLP)
+              </h2>
+              <h4 className='inter'>
+                Desarrollador de software
+              </h4>
+              <h6 className='inter'>
+                March 2024-December 2024
+              </h6>
+            </div>
+            <div style={{'flex': 5}}>
+              <h4>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;I was in charge of developing an AR experience where an interactive 3D model was rendered on a business card.
+              </h4>
+              <div className='multipleLinkContainer'>
+                <Link href='https://github.com/TevesManuel/UNLP-UNITEC-RA' target="_blank">More info</Link>
+              </div>
+              <h4>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;I was in charge of refactoring and optimizing the old code of a medical device for sensing diastolic pressure, systolic pressure, beats per minute and temperature and also migrated it from Arduino to PlatformIO. I developed the telemetry circuit together with a hardware abstraction for the SIM800L in order to facilitate the process of sending the collected data to the server via HTTP.
+              </h4>
+              <div className='multipleLinkContainer'>
+                <Link href='https://github.com/TevesManuel/UNLP-UNITEC-electromedicina' target="_blank">More information about the main project</Link>
+                <Link href='https://github.com/TevesManuel/SIM800L-HAL' target="_blank">More information about SIM800L hardware abstraction</Link>
+              </div>
+            </div>
+          </div>
+        </TimelineContent>
       </TimelineItem>
       <TimelineItem>
         <TimelineSeparator>
