a3c3be8b4643e0f6547fc32f76ba1e7c4b274e55
Manuel Teves <manuel.tomas.teves@gmail.com>
2024-10-21 21:04:54 -0300

    styling

--- src/components/MyExperience/MyExperience.tsx
+++ src/components/MyExperience/MyExperience.tsx
@@ -5,15 +5,15 @@ import WorkOutlineRounded from '@mui/icons-material/WorkOutlineRounded';
 const MyExperience : React.FC = () => {
     return (
         <div className='fw c'>
-        <div className='mainExperienceCotainer'>
-            <div className='titleExperience'>
-                <WorkOutlineRounded style={{'fontSize': '50px'}}/> <h1 className='inter'>&nbsp;Work experience</h1>
-            </div>
-            <div style={{'display': 'flex', 'alignItems': 'left'}}>
-                <MyExperienceTimeline />
+            <div className='mainExperienceCotainer'>
+                <div className='titleExperience'>
+                    <WorkOutlineRounded style={{'fontSize': '50px'}}/> <h1 className='inter'>&nbsp;Work experience</h1>
+                </div>
+                <div style={{'display': 'flex', 'alignItems': 'left', 'width': '70%'}}>
+                    <MyExperienceTimeline />
+                </div>
             </div>
         </div>
-    </div>
     );
 }
 
